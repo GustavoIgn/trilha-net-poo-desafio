7fe1dfe638d6308d5849f1206ee7c4d21d4af0c8
GustavoIgn <gustavoignacio280@gmail.com>
2024-01-02 19:53:38 -0300

    Desafio Feito - GSI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero: "987654321", modelo: "Modelo XV", imei: "121212121", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Discord");
+iphone.ReceberLigacao();
+
+Console.WriteLine("||||||||||||||||||||||||||||||");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "912345678", modelo: "Modelo VI", imei: "212121212", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("||||||||||||||||||||||||||||||");
+Console.WriteLine("||||||TESTES FINALIZADOS||||||");
